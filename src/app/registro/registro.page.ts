5ce702a2142345d401cc0a3631533dbc1e03b86c
Caroline Souza <carolinevmsouza@gmail.com>
2022-12-01 08:55:58 -0300

    Validação de CPF e Senha com ReactiveForms, exibindo mensagens de erro.

--- src/app/registro/registro.page.ts
+++ src/app/registro/registro.page.ts
@@ -10,6 +10,30 @@ export class RegistroPage implements OnInit {
 
   formCadastro: FormGroup;
 
+  mensagens = {
+    nome: [
+      { tipo: 'required', mensagem: 'O campo Nome é obrigatório!' },
+      { tipo: 'minlength', mensagem: 'O campo Nome precisa ter pelo menos 3 caracteres!' },
+    ],
+    cpf: [
+      {tipo: 'required', mensagem: 'O campo CPF é obrigatório!'},
+    ],
+  email: [
+    { tipo: 'required', mensagem: 'O campo E-mail é obrigatório.' },
+    { tipo: 'email', mensagem: 'E-mail Inválido.' },
+  ],
+  senha: [
+    { tipo: 'required', mensagem: 'É obrigatório confirmar senha.' },
+    { tipo: 'minlength', mensagem: 'A senha deve ter pelo menos 6 caracteres.', },
+    { tipo: 'maxlength', mensagem: 'A senha deve ter no máximo 8 caractéres.' },
+  ],
+  confirmaSenha: [
+    { tipo: 'required', mensagem: 'É obrigatório confirmar senha.' },
+    { tipo: 'minlength', mensagem: 'A senha deve ter pelo menos 6 caracteres.', },
+    { tipo: 'maxlength', mensagem: 'A senha deve ter no máximo 8 caractéres.' },
+    ],
+  };
+
   constructor(private formBiulder: FormBuilder) {
     this.formCadastro = this.formBiulder.group({
       nome: ['', Validators.compose([Validators.required, Validators.minLength(3)])],
